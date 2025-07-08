5398f3946f8bc8786a8f0fccc2f5e2c574a7f714
Leticia <leticiamineiro24@gmail.com>
2025-07-08 01:56:18 -0300

    Modificações no arquivo .txt

--- src/main/webapp/nav.js
+++ src/main/webapp/nav.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
   $(".sub-btn").click(function () {
     $(this).next(".sub-menu").slideToggle();
   });
-  $(".more-btn").click(function () {
-    $(this).next(".more-menu").slideToggle();
-  });
 });
 
 var menu = document.querySelector(".menu");
